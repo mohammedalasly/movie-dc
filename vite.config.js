04e040d43f33aae10ba2419d814d3ce1947c3e7b
mohammedalasly <mohammedalasly5@gmail.com>
2025-06-01 18:06:13 +0300

    config vite

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
-import { meta } from '@eslint/js';
+import { process } from '@eslint/js';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: meta.env.VITE_BASE_PATH || "/movie-dc",
+  base: process.env.VITE_BASE_PATH || "/movie-dc",
 });
